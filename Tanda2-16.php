8ba94f724265556e242db51ff994e83ce377a5b2
DESKTOP-DOOLO0V\Jesu\github <jesusrosadopereira@gmail.com>
2020-11-25 22:09:32 +0100

    1.0

---
+++ Tanda2-16.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>T2-09-02</title>
+  </head>
+  <body>
+        <?php
+          echo "La última cifra de tu numero es ". $_POST['n']%10;
+        ?>
+        <br><br>
+  </body>
+</html>
